bb37db0f7ad1a56a559e630fba625a4cb5d3c9dc
Jan Obladen <obladen@datenwelt.net>
2017-04-22 22:44:53 +0200

    Initial commit

--- src/lib/pemreader.js
+++ src/lib/pemreader.js
@@ -1,3 +1,280 @@
-/**
- * Created by job on 22.04.17.
- */
+const crypto = require('crypto');
+const VError = require('verror');
+
+const BEGIN_RSA_PRIVATE_KEY = '-----BEGIN RSA PRIVATE KEY-----';
+const END_RSA_PRIVATE_KEY = '-----END RSA PRIVATE KEY-----';
+
+function Parser(input) {
+	
+	this.input = input;
+	this.pos = 0;
+	this.eof = false;
+	
+}
+
+Parser.prototype.constructor = Parser;
+
+Parser.prototype.readline = function () {
+	if (this.eof || this.pos > this.input.length) {
+		this.eof = true;
+		return null;
+	}
+	let lineEnd = this.input.indexOf('\n', this.pos);
+	if (lineEnd === -1) {
+		lineEnd = this.input.length - 1;
+	}
+	lineEnd++;
+	let line = this.input.substring(this.pos, lineEnd);
+	this.pos = lineEnd;
+	line = line.trim();
+	return line;
+};
+
+Parser.prototype.skipUntil = function (match) {
+	const lineStart = this.pos;
+	while (!this.eof) {
+		const line = this.readline();
+		if (typeof match === 'string' && !line.startsWith(match)) continue;
+		if (!line.match(match)) continue;
+		this.pos = lineStart;
+		return line;
+	}
+	return null;
+};
+
+const PEMReader = {
+	
+	/**
+	 * Key derivation algorithm from OpenSSL. Generates a key from a passphrase with the
+	 * required byte length for use by a given cipher algorithm. Supported algorithms are:
+	 *
+	 * <ol>
+	 *     <li>AES-128-CBC (default)
+	 *     <li>AES-192-CBC
+	 *     <li>AES-256-CBC
+	 *     <li>DES-CBC
+	 *     <li>DES-EDE3-CBC (TripleDES)
+	 *  </ol>
+	 *
+	 *  The cipher algorithms determine default key lengths and sizes of the IV. Both parameters can be
+	 *  customized in the <code>options</code> parameter. If both parameters are present in the <code>options</code>
+	 *  parameter, the cipher algorithm is ignored.
+	 *
+	 * @param {(string|Buffer)} passphrase  The passphrase as a string or Buffer.
+	 * @param {object} [options]
+	 * @param {string} [options.cipher=AES-128-CBC] The cipher algorithm which determines key and IV sizes.
+	 * @param {string} [options.hash=MD5] The hashing algorithm to use.
+	 * @param {string|Buffer} [options.salt] The salt which is either empty or has least 8 bytes. If a string is specified,
+	 *                                       it is interpreted as a hex string and converted to byte values.
+	 * @param {Number} [options.count=1] The number of hash rounds to perform during key derivation.
+	 * @param {Number} [options.keyLength] The desired key length in bytes. Overrides the default value from options.cipher.
+	 * @param {Number} [options.ivLength] The desired iv length in bytes. Overrides the default value from options.cipher.
+	 *
+	 * @see https://wiki.openssl.org/index.php/Manual:EVP_BytesToKey(3)
+	 */
+	evpBytesToKey: function (passphrase, options) {
+		options = Object.assign({
+			cipher: 'AES-128-CBC',
+			hash: 'MD5',
+			count: 1,
+			salt: Buffer.alloc(0),
+		}, options || {});
+		
+		// Key lengths and IV sizes for known algorithms.
+		const configs = {
+			'AES-128-CBC': {
+				keyLength: 16,
+				ivLength: 16
+			},
+			'AES-192-CBC': {
+				keyLength: 24,
+				ivLength: 16
+			},
+			'AES-256-CBC': {
+				keyLength: 32,
+				ivLength: 16
+			},
+			'DES-CBC': {
+				keyLength: 8,
+				ivLength: 8
+			},
+			'DES-EDE3-CBC': {
+				keyLength: 24,
+				ivLength: 8
+			},
+			'SEED-CBC': {
+				keyLength: 16,
+				ivLength: 16
+			}
+		};
+		
+		// Convert passphrase to a byte buffer.
+		if ( typeof passphrase === 'string' ) {
+			passphrase = passphrase.length ? Buffer.from(passphrase) : Buffer.alloc(0);
+		}
+		if (!Buffer.isBuffer(passphrase)) {
+			throw new VError('Parameter "passphrase" must be a string or a buffer.');
+		}
+		
+		// Hash rounds (i.e. the "count" parameter from the EVP_bytesToKey()).
+		let hashRounds = options.count;
+		if ( hashRounds <= 0 ) {
+			throw new VError('Parameter "options.count" must be 1 or greater.');
+		}
+		hashRounds = Math.floor(hashRounds);
+		
+		const cipherAlgo = options.cipher;
+		const hashAlgo = options.hash;
+		
+		const config = configs[cipherAlgo.toUpperCase()];
+		if (!config) {
+			throw new VError('Unsupported cipher "%s". Supported: %j', cipherAlgo, configs.keys());
+		}
+		
+		// Override key and IV length if defined in the options parameter.
+		let keyLength = options.keyLength || config.keyLength;
+		let ivLength = options.ivLength || config.ivLength;
+		
+		// Create an 8 byte buffer for salt if there was a salt option. If no salt is used,
+		// create an empty buffer instead.
+		let salt = options.salt;
+		if (typeof salt === 'string') {
+			salt = !salt.length ? Buffer.alloc(0) : Buffer.from(salt, 'hex');
+		}
+		if (!Buffer.isBuffer(salt)) {
+			throw new VError('Parameter "options.salt" must be a hex string or a Buffer');
+		}
+		if (salt.length > 0 && salt.length < 8) {
+			throw new VError('Parameter "options.salt" must be empty or represent at least 8 bytes: options.salt = %d bytes', salt.length);
+		}
+		if (salt.length > 8) {
+			salt = salt.slice(0, 8);
+		}
+		
+		// Find size of hash by digesting some crap.
+		let hash = crypto.createHash(hashAlgo);
+		hash.update('XXX');
+		const hashLength = hash.digest().length;
+		
+		// How many bytes are needed and how many iterations are required to get there?
+		const bytesNeeded = keyLength + ivLength;
+		const iterations = Math.ceil(bytesNeeded / hashLength);
+
+		// Generate the key and IV bytes.
+		let keyAndIv = Buffer.alloc(bytesNeeded);
+		let d_prev = Buffer.alloc(0);
+		let iteration = 0;
+		while (iteration < iterations) {
+			// Quote:
+			//   D_i = HASH^count(D_(i-1) || data || salt)
+			let hash = crypto.createHash(hashAlgo);
+			hash.update(d_prev);
+			hash.update(passphrase);
+			hash.update(salt);
+			let d_curr = hash.digest();
+			let hashRound = 1;
+			while (hashRound++ < hashRounds) {
+				hash = crypto.createHash(hashAlgo);
+				hash.update(d_curr);
+				d_curr = hash.digest();
+			}
+			// Quote:
+			//   The key and IV is derived by concatenating D_1, D_2, etc until enough data is available for the
+			//   key and IV.
+			d_curr.copy(keyAndIv, iteration * hashLength);
+			d_prev = d_curr;
+			iteration++;
+		}
+		let key = keyAndIv.slice(0, keyLength);
+		let iv = keyAndIv.slice(keyLength, keyLength+ivLength);
+		return { key: key, iv: iv};
+	},
+	
+	readPrivateKey: function (bufferOrString, passphrase) {
+		let input = bufferOrString;
+		if (Buffer.isBuffer(bufferOrString)) {
+			input = bufferOrString.toString('utf8');
+		}
+		const parser = new Parser(input);
+		if (!parser.skipUntil(BEGIN_RSA_PRIVATE_KEY)) {
+			throw new VError('The input does not contain a PEM encoded private key');
+		}
+		
+		let privateKey = parser.readline();
+		
+		let line = parser.readline();
+		if (!line) {
+			throw new VError('The input ended unexpectedly after %s', BEGIN_RSA_PRIVATE_KEY);
+		}
+		let matches = line.match(/^Proc-Type:\s+(.+)/);
+		if (matches) {
+			// Encrypted private key.
+			
+			// Read Proc-Type and DEK-Info headers to find algorithm and IV.
+			const proctype = matches[1].trim().toUpperCase();
+			if (proctype !== '4,ENCRYPTED') {
+				throw new VError('Unknown Proc-Type value: %s', proctype);
+			}
+			let line = parser.skipUntil('DEK-Info:');
+			if (!line) {
+				throw new VError('Missing DEK-Info for Proc-Type "%s"', proctype);
+			}
+			matches = line.match(/^DEK-Info:\s+(.+)(?:,(.+))/);
+			if (!matches) {
+				throw new VError('Unparseable DEK-Info: %s', line);
+			}
+			if (matches.length !== 3) {
+				throw new VError('Missing IV in DEK-Info: %s', line);
+			}
+			const algo = matches[1];
+			const iv = Buffer.from(matches[2], 'hex');
+			
+			// Skip to end of header.
+			while (line = parser.readline()) {
+				if (line === "") break;
+			}
+			if (line !== "") {
+				throw new VError('Missing empty line after PEM header.');
+			}
+			
+			let { key: keyBuffer } = PEMReader.evpBytesToKey(passphrase, { cipher: algo, salt: iv });
+			let decipher = crypto.createDecipheriv(algo, keyBuffer, iv);
+			
+			let data = "";
+			while (line = parser.readline()) {
+				if (line.startsWith(END_RSA_PRIVATE_KEY)) break;
+				data += decipher.update(line, 'base64', 'base64');
+			}
+			data += decipher.final('base64');
+			privateKey = BEGIN_RSA_PRIVATE_KEY + "\n";
+			let pos = 0;
+			while (pos < data.length) {
+				privateKey += data.charAt(pos++);
+				if (pos % 64 === 0)
+					privateKey += "\n";
+			}
+			if (!privateKey.endsWith("\n"))
+				privateKey += "\n";
+			privateKey += END_RSA_PRIVATE_KEY;
+		} else {
+			// Unencrypted private key.
+			privateKey += "\n" + line + "\n";
+			while (line = parser.readline()) {
+				privateKey += line;
+				if (line.startsWith(END_RSA_PRIVATE_KEY)) {
+					break;
+				}
+				privateKey += "\n";
+			}
+			if (!line)
+				throw new VError('Missing %s after %s ', END_RSA_PRIVATE_KEY, BEGIN_RSA_PRIVATE_KEY);
+		}
+		return privateKey;
+	},
+	readPublicKey: function (buffer) {
+	
+	}
+	
+};
+
+module.exports = PEMReader;
--- test/lib/pemreader.test.js
+++ test/lib/pemreader.test.js
@@ -1,3 +1,107 @@
-/**
- * Created by job on 22.04.17.
- */
+const Promise = require('bluebird');
+
+const PEM = require('../../src/lib/pemreader');
+const fs = Promise.promisifyAll(require('fs'));
+
+const chai = require('chai');
+const expect = chai.expect;
+
+describe("lib/pemreader.js", function () {
+	
+	const unencryptedPrivateKey = `-----BEGIN RSA PRIVATE KEY-----
+MIIEowIBAAKCAQEA0ukwklDp1CUNOhZCraBZ8vT8EKVvXFNbJtkrX+tBF2zFckw8
+l099Gx+lab8nklsvY8nh/odUMIOv8tvm+tDUDe7kMkkgKsgREnn0mQQj3uU9KzxL
+BEYuFpWKP2sqMGv2Xp6vhPWDNgzUykN4Q0MgOc8BmFDuoIKepcnvaKU4l3rg6dW4
+RMaEWb0EUTg2rX9ESmsNKu7io6I7esPoWS4tJtBUJq7IWQl5Wpj/P3qMS01QZbZz
+hvzOSdUyp3CVHA8aDyAJ3H++e+wsOGUBlh7Tpxh1yxFIPBtZH7P7gs90V2AKXwn1
+j9oZwZ7Ox7tBL7Eag/Wk4vEx2K3saSNI54GFSwIDAQABAoIBADiaBEMAHACnAOm5
+16MpCXq+bHc7LqukYy4F0jInvIxA0Kxf8VLaLkT9NTMv7brFZe5t24ynXNy4Opv+
+j/p32LD0j6E/JrbUgmBnAlGeXtIOy1+zCp7XBr9g2n0ykVv1XWT/STNxgAkdYG33
+tWq3tQpOl6r87U1+QH1VuD5pOMrN3iln9cEwh1PPkML1py8QVW68dkzTfnuD7lm3
+bcCWGiUNN17OsaiaIa/paVLXR3v8lB01slVvC+LmJymp2xE0Osdqt4kVzqaakNsN
+f54rWuBwGgt+yXjVzdjlkWSEM0sQ8VZ1lHSbdCCsPAnbU/Iqv+fWAT8ZbbPLF9Pv
+RmFgWmECgYEA/RiCc0dZxQr/7b4QhihkggxuX0jKGC0KgvAzpCqesHESFWNQVoss
+LlAGzIbzUkvZhfxc9wwlP0htNYGkJNncMGnNmUvU0mEM9ZPaSx9pWZoN9FcEKyRs
+w/Th1Dm5YzDEdqgdL7HdBOChti84vl+4UyiEcqqrwOI1GPXuS41LC+UCgYEA1VTC
+MZXfq0ZBIFG8tJXDTYWDgVM989+H6mnCSU4eYZdNdsWcCT+h37WUrwUjk2DqCX9j
+n5mfAZ2G0PXPKg6f9joOZN2PCbvWMm1fLrIrAsIRDG8XHRkW/jfBLXwfj1PXp94u
+lDsHJKexg6J5k6zESn4dSICo7h8KcYkcgL+2GW8CgYABB/eIFrNT7S6Lvml80m9O
+ZFSSyM4h+RUA4Y+kTJQm7d7//U6Xe6uraKIOdUJKhIqXVCbgzsjcECR5wlRz3fta
+qyOuSzPLw5905KxQAAnC1rDV3QDkWBqlVbsRzUZnFAzI2vh7eEMJQQ0Hm/ukMumi
+VRjtCqr2dzV2K5JT9nhmzQKBgDCGLgHPYGf5+/hY/z5oTltEwkvZXl8peiX01m+M
+KFb0bndUrZBg+/YBvIcu+Q+d3L46TrPP4p4gcbj4IJ4lY3dDb2C8ELwbELa5sAvU
+FPp0oCIe5rgEq/k5P2SUNi2I2aXiYN/wqUhKKJOTkmnnw2JaW1jRgxteziZTFLuo
+X8hbAoGBAL2qMbG/kxTE3f8jdnn7z9MoZZism0JzlGXx0q81hXRruRiGDZ5hJyLv
+tq0LbuT1N38IOpBDtqfMHlHHk9SRK/Mx0b6a0uATSb+1TYIZdgF3wdKmm9h4dVdy
+Z2n/LW9xVrQQiP+xiEWTlzgPiQXt/uynBCwPTWYino5Rsu3He5P0
+-----END RSA PRIVATE KEY-----`;
+	
+	describe("PEMReader.evpBytesToKey()", function () {
+		
+		it("generates the expected key and IV for AES-128-CBC", function () {
+			const salt = "13F689619F77E4E55F68556C1A9FEEF8";
+			const passphrase = "test123456";
+			const { key, iv } = PEM.evpBytesToKey(passphrase, {cipher: 'AES-128-CBC', salt: salt});
+			expect(key.toString('hex')).to.equal('747783301f72d2ddbe5a19c8f1e08254');
+			expect(iv.toString('hex')).to.equal('fa5277af059220de3ca0f9a2d2c30a1d');
+		});
+		
+		it("generates the expected key and IV for AES-192-CBC", function () {
+			const salt = "13F689619F77E4E55F68556C1A9FEEF8";
+			const passphrase = "test123456";
+			const { key, iv } = PEM.evpBytesToKey(passphrase, {cipher: 'AES-192-CBC', salt: salt});
+			expect(key.toString('hex')).to.equal('747783301f72d2ddbe5a19c8f1e08254fa5277af059220de');
+			expect(iv.toString('hex')).to.equal('3ca0f9a2d2c30a1da0ed7665f278bdd6');
+		});
+		
+		it("generates the expected key and IV for AES-256-CBC", function () {
+			const salt = "13F689619F77E4E55F68556C1A9FEEF8";
+			const passphrase = "test123456";
+			const { key, iv } = PEM.evpBytesToKey(passphrase, {cipher: 'AES-256-CBC', salt: salt});
+			expect(key.toString('hex')).to.equal('747783301f72d2ddbe5a19c8f1e08254fa5277af059220de3ca0f9a2d2c30a1d');
+			expect(iv.toString('hex')).to.equal('a0ed7665f278bdd69710ed632cb3917d');
+		});
+		
+		it("generates the expected key and IV for DES-CBC", function () {
+			const salt = "13F689619F77E4E55F68556C1A9FEEF8";
+			const passphrase = "test123456";
+			const { key, iv } = PEM.evpBytesToKey(passphrase, {cipher: 'DES-CBC', salt: salt});
+			expect(key.toString('hex')).to.equal('747783301f72d2dd');
+			expect(iv.toString('hex')).to.equal('be5a19c8f1e08254');
+		});
+		
+		it("generates the expected key and IV for DES-EDE3-CBC", function () {
+			const salt = "13F689619F77E4E55F68556C1A9FEEF8";
+			const passphrase = "test123456";
+			const { key, iv } = PEM.evpBytesToKey(passphrase, {cipher: 'DES-EDE3-CBC', salt: salt});
+			expect(key.toString('hex')).to.equal('747783301f72d2ddbe5a19c8f1e08254fa5277af059220de');
+			expect(iv.toString('hex')).to.equal('3ca0f9a2d2c30a1d');
+		});
+		
+		it("generates the expected key and IV for arbitrary key and IV sizes with an alternative hash", function () {
+			const salt = "13F689619F77E4E55F68556C1A9FEEF8";
+			const passphrase = "test123456";
+			const { key, iv } = PEM.evpBytesToKey(passphrase, {cipher: 'DES-EDE3-CBC', salt: salt, keyLength: 64, ivLength: 30, count: 10, hash: 'SHA1'});
+			expect(key.toString('hex')).to.equal('9e6e11a8015ba3bc13f0c58bdd286fd9a9128269a6f623accbd2678eec6f5a6d95ab375ebeb002a36fa662ade4132494f9895c21c4a9b73527b00390de1cf405');
+			expect(iv.toString('hex')).to.equal('43f447d993863c59cab29e3cf64d6d457b495feb2db635b8884286b61a40');
+		});
+		
+	});
+	
+	describe("PEMReader.readPrivateKey()", function () {
+		
+		it("reads an unencrypted private key", async function () {
+			const data = await fs.readFileAsync('test/data/rsa/privkey.pem');
+			const privkey = PEM.readPrivateKey(data);
+			expect(privkey).to.equal(unencryptedPrivateKey);
+		});
+		
+		it("reads an encrypted private key", async function () {
+			const data = await fs.readFileAsync('test/data/rsa/privkey.encrypted.pem');
+			const privkey = PEM.readPrivateKey(data, 'test123456');
+			expect(privkey).to.equal(unencryptedPrivateKey);
+		});
+		
+	});
+	
+});
